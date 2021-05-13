76c4ab28130c993c6559ee014bce8847ad185fbf
Federico Gnudi <federico.gnudi4@studio.unibo.it>
2021-05-13 22:39:23 +0200

    ho aggiornato alcune cose ma penso di reimpostare il programma

--- infection.hpp
+++ infection.hpp
@@ -1,62 +1,69 @@
 #ifndef INFECTION_PROJECT_HPP
 #define INFECTION_PROJECT_HPP
 
-#include <vector>
-#include <cassert>
 #include <math.h>
+#include <iostream>
+#include <cassert>
+#include <vector>
 
-struct State{
-   int S; //suscettibili
-   int M; //rimossi
-   int R; //malati
-   int V; //vaccinati
+struct State {
+  int S;  // suscettibili
+  int M;  // rimossi
+  int R;  // malati
+  int V;  // vaccinati
 };
 
-
 class Infection {
-   int m_duration_analysis_indays;  //durata analisi dati
-   State m_initial_state;
-   int const m_N;
+  int m_duration_analysis_indays;  // durata analisi dati
+  State m_initial_state;
+  int const m_N; // abitanti
 
-   public:
+ public:
+  Infection(int duration_analysis_indays, State initial_state, int N)
+      : m_duration_analysis_indays{duration_analysis_indays},
+        m_initial_state{initial_state},
+        m_N{N} {}
 
-   Infection(int duration_analysis_indays, State initial_state, int N):
-   m_duration_analysis_indays{duration_analysis_indays}, m_initial_state{initial_state}, m_N{N} {}
+  std::vector<State> SMRV() const {
+    // double beta, double gamma, int non_vaccinabl, int M_t0, int V_t0, double
+    // vel_vaccini, double eff_vaccino, int R_t0
+    double const beta = 0.056;
+    double const gamma = 0.045;
+    int const non_vaccinabl = 118292;  // eta
+    double const vel_vaccini = 0.05;   // mu
+    double const eff_vaccino = 0.839;  // efficacia vaccino pesata sui vaccini distribuiti
 
-   std::vector<State> SMRV() const {
-       //double beta, double gamma, int non_vaccinabl, int M_t0, int V_t0, double vel_vaccini, double eff_vaccino, int R_t0
-       double const beta = 0.056;
-       double const gamma= 0.045;
-       int const  non_vaccinabl= 118292; //eta
-       double const vel_vaccini=0.05; //mu
-       double const eff_vaccino=0.839; //efficacia vaccino pesata sui vaccini distribuiti 
-       
-       std::vector<State> result{m_initial_state};
+    std::vector<State> result{m_initial_state};
 
-       State support=result.back();
+    State support = result.back();
 
-       for (int i=1; i<m_duration_analysis_indays; ++i){
+    for (int i = 1; i < m_duration_analysis_indays; ++i) {
+      assert(result.back().S + result.back().M + result.back().R + result.back().V == m_N);  // assert con vincolo
 
-           assert(result.back().S + result.back().M + result.back().R + result.back().V == m_N); //assert con vincolo
-
-           support.V=round(result.back().V + vel_vaccini*(result.back().V / eff_vaccino)*(1- result.back().V / (eff_vaccino*(m_N-non_vaccinabl))));
-           support.M=round(result.back().M + beta*(result.back().S / m_N)*result.back().M - gamma*result.back().M);
-           support.S=round(result.back().S -beta*(result.back().S/m_N)*result.back().M - vel_vaccini*(result.back().V / eff_vaccino)*(1- result.back().V / (eff_vaccino*(m_N-non_vaccinabl))));
-           support.R=round(result.back().R + gamma*result.back().M);
-           result.push_back(support);
-       }
-       
+      support.V = round(result.back().V + vel_vaccini * (result.back().V / eff_vaccino) * (1 - result.back().V / (eff_vaccino * (m_N - non_vaccinabl))));
+      support.M = round(result.back().M + beta * (result.back().S / m_N) * result.back().M - gamma * result.back().M);
+      support.S = round(result.back().S - beta * (result.back().S / m_N) * result.back().M - vel_vaccini * (result.back().V / eff_vaccino) *
+                  (1 - result.back().V / (eff_vaccino * (m_N - non_vaccinabl))));
+      support.R = round(result.back().R + gamma * result.back().M);
+      result.push_back(support);
     }
 
-    void print() const{
-        
+    return result;
+  }
 
+  void tabulate(std::ostream& os, Infection const& inf) {
+    std::vector<State> datas=inf.SMRV();
+    int const N= 52;
+    
+    os << '+' << std::string(N, '-') << "+\n";
+    os << "| DAYS | MALATI | RIMOSSI | VACCINATI | SUSCETTIBILI |\n";
+    for (int r = 0; r < inf.m_duration_analysis_indays; ++r) {
+      os << "| " << r+1 << " | " << datas[r].M << " | " << datas[r].R << " | " << datas[r].V << " | " << datas[r].S << " |\n";
     }
-
-
-
-
-
-}; 
+    os << '+' << std::string(N, '-') << "+\n";
+  }
+}
+}
+;
 
 #endif
