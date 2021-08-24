c54df5c80776b611670ae618589af15f053c99a2
Giulio Pastorello <giulio.pastorello@gmail.com>
2021-08-24 08:53:14 +0200

    oggi conquisto sfml

--- parte 2/world.hpp
+++ parte 2/world.hpp
@@ -6,7 +6,7 @@
 
 namespace covid {
 
-    enum class Cell : char { S, M, R, V};
+    enum class Cell : char { S, I, R, V};
 
 class World 
 {
@@ -15,14 +15,21 @@ class World
     Grid m_grid;
 
   public:
-    World(int n) : m_side(n), m_grid(m_side * m_side, Cell::S)
-    {    
-    }
+    World(int n);
     int side() const
     {
         return m_side;
     }
     Cell const& cell(int r, int c) const noexcept 
+    {
+        auto const i = (r + m_side) % m_side; //row index
+        auto const j = (c + m_side) % m_side; //column index
+        assert(i >= 0 && i < m_side && j >= 0 && j < m_side);
+        auto const index = i * m_side + j;
+        assert(index >= 0 && index < static_cast<int>(m_grid.size()));
+        return m_grid[index];
+    }
+    Cell& cell(int r, int c) noexcept
     {
         auto const i = (r + m_side) % m_side;
         auto const j = (c + m_side) % m_side;
@@ -30,11 +37,18 @@ class World
         auto const index = i * m_side + j;
         assert(index >= 0 && index < static_cast<int>(m_grid.size()));
         return m_grid[index];
+    }  
+
+    friend bool operator==(World const& l, World const& r)
+    {
+        return l.m_grid == r.m_grid;
     }
-    
- 
 };
 
+inline int neighbours_infected(World const& world, int r, int c)
+{
+    ;
+}
 }
 
 #endif
