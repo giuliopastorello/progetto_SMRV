c42bf5a9322f007d3d5e3b1a09abf687df1461d0
Giulio Pastorello <giulio.pastorello@gmail.com>
2023-10-12 13:09:45 +0200

    state vectore members getters
    necessary for plotting w/ matplotlib++

--- display.cpp
+++ display.cpp
@@ -1,54 +1,63 @@
-#include "infection.hpp"
 #include "display.hpp"
 
 #include <matplot/matplot.h>
 
-#include "include/termcolor.hpp"
+#include <cassert>
 
-namespace epidemic
-{
+namespace epidemic{
 
-    void print(Infection const &simulation)
-    {
-        std::vector<State> states = simulation.states();
+std::vector<int> get_S(std::vector<State> const &states) {
+  int const N = states.size();
+  std::vector<int> result{};
+  result.reserve(N);
 
-        int N = states[0].S + states[0].M + states[0].R +
-                states[0].V;
-        int const width = std::log10(N) + 4;
-
-        std::cout << '\n'
-                  << '+' << std::string(width - 1, '-') << '+'
-                  << std::string(width - 1, '-') << '+' << std::string(width - 1, '-')
-                  << '+' << std::string(width, '-') << '+' << '\n'
-                  << '|' << 'S' << std::string(width - 2, ' ')
-                  << '|' << 'M' << std::string(width - 2, ' ')
-                  << '|' << 'R' << std::string(width - 2, ' ')
-                  << '|' << 'V' << std::string(width - 1, ' ')
-                  << '|' << '\n'
-                  << '+' << std::string(width - 1, '-') << '+'
-                  << std::string(width - 1, '-') << '+' << std::string(width - 1, '-')
-                  << '+' << std::string(width, '-') << '+' << '\n';
-
-  for (State const& state : states) {
-    simulation.print(state);
+  for (int i = 0; i < N; ++i) {
+    int num_s = states[i].S;
+    result.push_back(num_s);
   }
+  assert(states.size() == result.size());
+  return result;
+}
 
-    } 
+std::vector<int> get_M(std::vector<State> const &states) {
+  int const N = states.size();
+  std::vector<int> result{};
+  result.reserve(N);
 
-  //  std::vector<int> get_S(Infection const &simulation) {
-  //  
-  //  std::vector<State> states = simulation.states();
-  //  
-  //  int const N = states.size();
-  //  std::vector<int> result{};
-  //  result.reserve(N);
-//
-  //  for (State const& state : states) {
-  //    int num_s = virus.state().S;
-  //    result.push_back(num_s);
-  //  }
-  //  assert(states.size() == result.size());
-  //  return result;
-//}
+  for (int i = 0; i < N; ++i) {
+    int num_m = states[i].M;
+    result.push_back(num_m);
+  }
+  assert(states.size() == result.size());
+  return result;
+}
 
-} 
+std::vector<int> get_R(std::vector<State> const &states) {
+  int const N = states.size();
+  std::vector<int> result{};
+  result.reserve(N);
+
+  for (int i = 0; i < N; ++i) {
+    int num_r = states[i].R;
+    result.push_back(num_r);
+  }
+  assert(states.size() == result.size());
+  return result;
+}
+
+std::vector<int> get_V(std::vector<State> const &states) {
+  int const N = states.size();
+  std::vector<int> result{};
+  result.reserve(N);
+
+  for (int i = 0; i < N; ++i) {
+    int num_v = states[i].V;
+    result.push_back(num_v);
+  }
+  assert(states.size() == result.size());
+  return result;
+}
+
+void graph(std::vector<State> const &states){}
+}
+
--- display.hpp
+++ display.hpp
@@ -3,15 +3,9 @@
 
 #include "infection.hpp"
 
-namespace epidemic
-{
-    void print(Infection const &simulation);
+namespace epidemic{
 
-    std::vector<int> get_S(std::vector<State> const &simulation);
-    std::vector<int> get_I(std::vector<State> const &simulation);
-    std::vector<int> get_R(std::vector<State> const &simulation);
-    std::vector<int> get_D(std::vector<State> const &simulation);
-
-    //void plot(std::vector<State> const &simulation);
+void graph(std::vector<State> const &states);
 }
+
 #endif
