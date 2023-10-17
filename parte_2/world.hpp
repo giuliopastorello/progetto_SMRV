cc91b6f9c1436ac0723d2443adae37ef973c245c
Federico Gnudi <federico.gnudi4@studio.unibo.it>
2023-10-17 17:28:59 +0200

    aggiunta cartella parte 2

---
+++ parte_2/world.hpp
@@ -0,0 +1,47 @@
+#ifndef WORLD_HPP
+#define WORLD_HPP
+
+#include <vector>
+
+namespace GameofLife{
+
+enum class Cell {Empty, S, I, R };
+
+class World {
+
+   int m_side; //dimensione griglia m_side x m_side
+   std::vector<Cell> m_field; //vettore griglia di celle
+
+   public:
+
+   int side() const; //get per il side
+   int S_Number() const; //get numero di suscettibili
+   int I_Number() const; //get numero di infetti
+   int R_Number() const; //get numero di rimossi
+
+   void Set_cell(Cell const &cell_type,int r, int c); //cambia il valore della cella in posizione (r,c)
+   
+   Cell Get_cell(int r,int c) const;// restituisce la cella in posizione (r,c) riga colonna
+   
+   World(int a); //costruttore celle vuote
+   
+};//class world
+
+void initial_random(World &world, int num_s, int num_i); //metodo che prende una griglia creata e genera random infetti e suscettibili
+
+int infected_counter(World &World,int r,int c); //conta il numero di celle infette intorno 
+
+bool infected(int number_counter,double beta); //metodo per stabilire se contagiare
+  
+bool removed(double gamma); //metodo per stabilire se rimuovere
+
+bool move_condition(World const &World,int r,int c);
+
+World evolve(World &World, double beta, double gamma);
+
+void worldDisplay(World &World);//metodo per graficare su terminale
+
+}//namespace gameoflife
+
+
+#endif
---
+++ parte_2/worldtest.cpp
@@ -0,0 +1,47 @@
+#include "world.hpp"
+#include <iostream>
+#include <stdlib.h> 
+
+
+
+int main(){
+
+   system("clear");
+   
+   GameofLife::World world_test1(5);
+
+   GameofLife::initial_random(world_test1,4,3);
+   GameofLife::worldDisplay(world_test1);
+
+   int a=0;
+   GameofLife::World next(5);
+   
+   while(a<10){
+      next=GameofLife::evolve(world_test1,0.4,0.3);
+      GameofLife::worldDisplay(next);
+      world_test1=next;
+      a++;
+   }
+   /*
+   world_test.Set_cell(GameofLife::Cell::S,0,0);
+   world_test.Set_cell(GameofLife::Cell::I,1,1);
+   world_test.Set_cell(GameofLife::Cell::S,0,1);
+   world_test.Set_cell(GameofLife::Cell::I,3,3);
+
+   
+
+   std::cout<<GameofLife::infected_counter(world_test,0,0);
+   std::cout<<'\n';
+
+   GameofLife::worldDisplay(world_test);
+
+   GameofLife::World next1(4);
+   next1=GameofLife::evolve(world_test,0.4,0.2);
+   GameofLife::worldDisplay(next1);
+
+   GameofLife::World next2(4);
+   next2=GameofLife::evolve(next1,0.4,0.2);
+   GameofLife::worldDisplay(next2);
+   
+   */
+}
