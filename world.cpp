931775048116d6e27858478bd231127e76ffff1e
Federico Gnudi <federico.gnudi4@studio.unibo.it>
2023-10-20 17:39:06 +0200

    include per termcolor cambiato

--- world.cpp
+++ world.cpp
@@ -1,6 +1,5 @@
 #include "world.hpp"
-
-#include "termcolor.hpp"
+#include "include/termcolor.hpp"
 #include <algorithm> //contiene metodo count per ricerca nei vettori
 #include <cassert>
 #include <cmath>
