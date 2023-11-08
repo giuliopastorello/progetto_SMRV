3c1219be3d441ab186f499789b30a033435f7844
Federico Gnudi <federico.gnudi4@studio.unibo.it>
2023-11-08 14:15:35 +0100

    creazione world.test.cpp ; da testare implementadolo in cmake

---
+++ parte2/world.test.cpp
@@ -0,0 +1,33 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "world.hpp"
+
+#include "../include/doctest.h"
+
+TEST_CASE("Testing Virus") {
+    
+  using namespace VirusGame;
+  int const grid_size = 5;
+
+  SUBCASE("Testing Throws") {
+    int const n = -1;
+    CHECK_THROWS(World(n));
+
+    World world(grid_size);
+    CHECK_THROWS(initial_random(world, 10, 30));
+
+    World world2(grid_size);
+    CHECK_THROWS(initial_random(world2, 10, 0));
+  }
+
+  SUBCASE("Testing infected_counter") {
+    World world(grid_size);
+    world.Set_cell(Cell::Infected, 1, 0);
+    world.Set_cell(Cell::Infected, 2, 2);
+    world.Set_cell(Cell::Infected, 3, 2);
+
+    int const infected_around = infected_counter(world, 2, 1);
+
+    CHECK(infected_around == 3);
+  }
+
+}
